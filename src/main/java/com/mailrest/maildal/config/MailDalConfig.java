8c9499d07e40fc169151084caa65926222816dd6
Albert Shift <a@albertshift.com>
2015-06-06 15:36:43 -0700

    maildal config impl

--- src/main/java/com/mailrest/maildal/config/MailDalConfig.java
+++ src/main/java/com/mailrest/maildal/config/MailDalConfig.java
@@ -4,6 +4,9 @@
  */
 package com.mailrest.maildal.config;
 
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.policies.ConstantReconnectionPolicy;
+import com.mailrest.maildal.model.Account;
 import com.noorq.casser.core.Casser;
 import com.noorq.casser.core.CasserSession;
 
@@ -11,8 +14,18 @@ public final class MailDalConfig {
 
 	private final CasserSession session;
 	
-	public MailDalConfig(String host) {
-		session = Casser.session();
+	public MailDalConfig(String host, String keyspace) {
+		
+		Cluster cluster = Cluster.builder()
+			.addContactPoint(host)
+			.withReconnectionPolicy(new ConstantReconnectionPolicy(1000))
+			.build();
+		
+		session = Casser.connect(cluster, keyspace)
+			.addPackage(Account.class.getPackage().getName())
+			.autoUpdate()
+			.showCql()
+			.get();
 	}
 	
 	public CasserSession session() {
