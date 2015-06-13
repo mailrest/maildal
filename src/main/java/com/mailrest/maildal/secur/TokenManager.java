9deeec5da8d173635215e79d7549e0981a6b0a07
Albert Shift <a@albertshift.com>
2015-06-12 17:11:46 -0700

    add TokenLink converter

--- src/main/java/com/mailrest/maildal/secur/TokenManager.java
+++ src/main/java/com/mailrest/maildal/secur/TokenManager.java
@@ -9,5 +9,15 @@ public interface TokenManager<T> {
 	String toJwt(T tokenObj, int durationMinutes);
 	
 	T fromJwt(String jwt); 
+
+	default String toJwtLink(T tokenObj, int durationMinutes) {
+		String jwt = toJwt(tokenObj, durationMinutes);
+		return TokenLink.INSTANCE.encode(jwt);
+	}
+	
+	default T fromJwtLink(String jwtLink) {
+		String jwt = TokenLink.INSTANCE.decode(jwtLink);
+		return fromJwt(jwt); 
+	}
 	
 }
